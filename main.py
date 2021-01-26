3b94eb7419dc04ea90116f2a94a6888be80d32c8
Kieran <ngbenson1998@gmail.com>
2021-01-27 00:52:44 +0800

    Added main.py
    Added __name__ conditional

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+# Note that this is a linting error not a program error
+# pylint: disable=no-member
+
+def main():
+    print("Main")
+    pygame.init()
+    
+if __name__ == "__main__":
+    main()
