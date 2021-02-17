8c379d9e5630e412b270870d3415326bc864339a
Charissa <charissa.utomo@gmail.com>
2021-02-17 16:00:49 +0800

    BST and README edit

--- BST.py
+++ BST.py
@@ -11,54 +11,71 @@ class Node:
         self.data = data
         self.leftchild = None 
         self.rightchild = None
+        self.head = None
+       
     
-class BST:
-    def insert(bst,data):
-        if bst == None:
-            return Node(data)
+class BST: 
+    def __init__(self):
+         self.head = None
+
+    def insert(self,data):
+        if self == None:
+            self.head = Node(data)
+            return self.head
         else:
-            if bst.data == data:
-                return bst
-            elif bst.data < data:
-                bst.rightchild = BST.insert(bst.rightchild,data)
+            if self.data == data:
+                return self.data
+            elif self.data < data:
+                self.rightchild = BST.insert(self.rightchild,data)
             else:
-                bst.leftchild = BST.insert(bst.leftchild,data)
-        return bst
-    def inordertraversal(bst): 
-        if bst != None:
-            BST.inordertraversal(bst.leftchild)
-            print(bst.data)
-            BST.inordertraversal(bst.rightchild)
+                self.leftchild = BST.insert(self.leftchild,data)
+            return self.data
+    
+    def inordertraversal(self): 
+        if self != None:
+            BST.inordertraversal(self.leftchild)
+            print(self.data)
+            BST.inordertraversal(self.rightchild)
         else:
-            return bst
+            return self.data
+        
     def minval(node):
         curr = node
         while curr.left != None:
             curr = curr.leftchild
         return curr
-    def delete(bst,data):
-        if bst != None:
-            if data < bst.data:
-                bst.leftchild = BST.delete(bst.leftchild,data)
-            elif data > bst.data:
-                bst.rightchild = BST.delete(bst.rightchild,data)
+    
+    def delete(self,data):
+        if self != None:
+            if data < self.data:
+                self.leftchild = BST.delete(self.leftchild,data)
+            elif data > self.data:
+                self.rightchild = BST.delete(self.rightchild,data)
             else:
-                if bst.leftchild == None:
-                    temp = bst.rightchild
-                    bst = None
+                if self.leftchild == None:
+                    temp = self.rightchild
+                    self.data = None
                     return temp
-                elif bst.rightchild == None:
-                    temp = bst.leftchild
-                    bst = None
+                elif self.rightchild == None:
+                    temp = self.leftchild
+                    self.data = None
                     return temp
-                temp = BST.minval(bst.rightchild)
-                bst.data = temp.data
-                bst.rightchild = BST.delete(bst.rightchild,temp.data)
-            return bst
+                temp = self.minval(self.rightchild)
+                self.data = self.data
+                self.rightchild = self.delete(self.rightchild,temp.data)
+            return self.data
+    def search(self,key):
+        if self.head == None or self.data == key:
+            return self.head
+ 
+        if self.data < key:
+            return BST.search(self.head.rightchild,key)
+        else:
+            return BST.search(self.head.leftchild,key)
 #example that the BST works
 bst1 = None
 bst1 = BST.insert(bst1,40)   
-bst1 = BST.insert(bst1,90)   
+bst1 = BST.insert(bst1,90)     
 bst1 = BST.insert(bst1,64)  
 bst1 = BST.insert(bst1,54)  
 bst1 = BST.delete(bst1,64)
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # SOC
-Space Optimization For COVID-19 Wards
+##**Space Optimization For COVID-19 Wards**
+
+###Welcome! :wave:
+
+
+@KieranNg and I are Mathematical Science students from NTU under SPMS.<br/>
+We are working on a project for SCOPE Odyessey. We are trying to optimize a given ward space and equally divide them into subsections.<br/>
+
+
+Listed below are the main features of SOC:
+'''
+- [Voronoi Events](https://github.com/charutomo/SOC/blob/Renderer/VoronoiEvent.py)
+- [Rooms](https://github.com/charutomo/SOC/blob/Renderer/room.py)
+- [Circumcircle](https://github.com/charutomo/SOC/blob/BST_Queue/Circumcircle%20Coefficients.py)
+'''
+
+Our report will be released on later date.<br/>
+Thank you.
