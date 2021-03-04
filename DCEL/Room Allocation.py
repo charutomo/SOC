9dec1ae093a5e750ff9e4d42288f36f9c6857e86
Charissa <charissa.utomo@gmail.com>
2021-03-04 23:06:45 +0800

    DCEL Edits
    Made some changes to DCEL, still wip as there is Nonetype error that need to kiv, trying to consider the pathways widths.

--- DCEL/Room Allocation.py
+++ DCEL/Room Allocation.py
@@ -6,23 +6,50 @@ Created on Tue Mar 2 23:58:58 2021
 """
 import math 
 
-def findHAngle(dx, dy):
-  """Determines the angle with respect to the x axis of a segment
-  of coordinates dx and dy
-  """
-  l = math.sqrt(dx*dx + dy*dy)
-  if dy > 0:
-    return math.acos(dx/l)
-  else:
-    return 2*math.pi - math.acos(dx/l)
+def findHAngle(x, y):
+ 
+    '''
+     Find the angle wrt the x axis with delta x and y coordinates
+
+    Parameters
+    ----------
+    x : x coordinates
+    y : y coordinates
+
+    Returns
+    -------
+    angle
+        return angle wrt the x axis with delta x and y coordinates
+
+    '''
+    l = math.sqrt(x**2 + y**2)
+    if y > 0:
+      return math.acos(x/l)
+    else:
+      return 2*math.pi - math.acos(x/l)
 
 class Vertex:
+    '''Vertex of DCEL'''
     def __init__(self, xcoordinate, ycoordinate):
         self.x = xcoordinate
         self.y = ycoordinate
         self.halfedge = []
     
     def equivalence(self,_others):
+        '''
+        To check if vertex is equivalent to other vertex
+
+        Parameters
+        ----------
+        _others : Vertex
+            
+
+        Returns
+        -------
+        bool
+            True if they are the same vertex and False if otherwise
+
+        '''
         if type(_others)== Vertex:
             if self.x == _others.x and self.y == _others.y:
                 return True
@@ -32,9 +59,18 @@ class Vertex:
             return False
 
     def sortededges(self):
+        '''
+        sort half edges according to the angles wrt to x axis
+
+        Returns
+        -------
+        sorted list of halfedges
+
+        '''
         self.halfedge.sort(key=lambda a: a.angle, reverse=True)
 
 class Edges:
+    '''Edge of DCEL'''
     def __init__(self,point1,point2):
         self.prev = None
         self.next = None
@@ -44,29 +80,72 @@ class Edges:
         self.angle = findHAngle(point2.x-point1.x,point2.y-point1.y)
     
     def equivalence(self,_others):
+        '''
+        To check if edges are equivalent
+        
+        Parameters
+        ----------
+        _others : Edge
+            
+
+        Returns
+        -------
+        bool
+            True if they are the same vertex and False if otherwise
+
+        '''
         if self.tail == _others.tail and self.next.tail == _others.next.tail:
             return True
         else:
             return False
     
     def printedge(self):
+        '''
+        produce edges coefficients
+
+        Returns
+        -------
+        self.tail.x, self.tail.y,self.next.tail.x,self.next.tail.y)
+            if they exists
+
+        '''
         if self.next !=None:
             return (self.tail.x, self.tail.y,self.next.tail.x,self.next.tail.y)
         else:
             return self.tail.x, self.tail.y
 
 class Face:
-     def __init__(self):
+    '''face of DCEL'''
+    
+    def __init__(self):
          self.halfEdge = None
          self.variable = None
    
 class DCEL:
-    def __init__(self):
+    '''Doubly Connected Edge List'''
+    def __init__(self,vertexlist,edgelist):
         self.vertices = []
         self.edges = []
         self.faces = []
+        self.vl = vertexlist
+        self.el = edgelist
     
     def findVertex(self, x, y):
+        '''
+        locate vertex with x annd y coordinate
+
+        Parameters
+        ----------
+        x : x coordinate
+
+        y : y coordinate
+
+        Returns
+        -------
+        vert : Vertex
+            returns vertex with give x and y coordinates
+
+        '''
         for vert in self.vertices:
             if vert.x == x and vert.y == y:
                 return vert
@@ -74,6 +153,22 @@ class DCEL:
             return None
     
     def findHalfEdge(self, v1, v2):
+        '''
+        find the half edge with vertices
+
+        Parameters
+        ----------
+        v1 : First vertex
+
+        v2 : Second Vertex
+
+
+        Returns
+        -------
+        h : Half edge
+            Half edge associated to the two given vertex
+
+        '''
         for h in self.edges:
             nextEdge = h.next
             if (h.tail.x == v1[0] and h.tail.y == v1[1]) and (nextEdge.tail.x == v2[0] and nextEdge.tail.y == v2[1]):
@@ -81,65 +176,89 @@ class DCEL:
         else:
             return None
     
-    def printDCEL(self, position, subdivide):
-        for point in position:
-            self.vertices.insert(-1,Vertex(point[0],point[1]))
-        for section in subdivide:
-            start = section[0]
-            end = section[1]
-            v1 = self.findVertex(start[0], start[1])
-            v2 = self.findVertex(end[0], end[1])
-            e1 = Edges(v1,v2)
-            e2 = Edges(v2,v1)
-            e1.twin = e2
-            e2.twin = e1
-            v1.halfedge.append(e1)
-            v2.halfedge.append(e2)
-            self.edges.append(e1)
-            self.edges.append(e2)
+    def printDCEL(self):
+        '''
+        generates DCEL with list of vertex and edges
+
+        Returns
+        -------
+        DCEL 
+
+        '''
+        for v in self.vl:
+            self.vertices.insert(-1,Vertex(v[0],v[1]))
+        for e in self.el:
+            start = e[0]
+            end = e[1]
+            if (start[0] and start[1]) >= 0 and (end[0] and end[1]) >= 0:
+                v1 = self.findVertex(start[0], start[1])
+                v2 = self.findVertex(end[0], end[1])
+                e1 = Edges(v1,v2)
+                e2 = Edges(v2,v1)
+                e1.twin = e2
+                e2.twin = e1
+                v1.halfedge.append(e1)
+                v2.halfedge.append(e2)
+                self.edges.append(e1)
+                self.edges.append(e2)
             
         for v in self.vertices:
             v.sortededges()
             halfedgecount = len(v.halfedge)
             if halfedgecount <2:
                 return ("DCEL cannot be produced as it is invalid, it requires at least 2 half edge. Please try again.")
-            for i in range(0, halfedgecount-1):
-                h1 = v.halfedge[i]
-                h2 = v.halfedge[i+1]
-                h1.twin.next = h2
-                h2.prev = h1.twin
-            h1 = v.halfedge[halfedgecount-1]
-            h2 = v.halfedge[0]
+            else:
+                for i in range(0, halfedgecount-1):
+                    h1 = v.halfedge[i]
+                    h2 = v.halfedge[i+1]
+                    h1.twin.next = h2
+                    h2.prev = h1.twin
+                h1 = v.halfedge[halfedgecount-1]
+                h2 = v.halfedge[0]
+             
             indexface = 0
-            for he in self.edges:
+            newlist = self.edges[:]
+            length = len(self.edges)
+            while length > 0:
+                he = newlist.pop()
+                length-=1
                 if he.face == None:
+                    face = Face()
                     indexface += 1
-                face = Face()
-                face.variable = str(indexface)
-                face.he = he
-                he.face = face
-                h = he
-                # You did not consider the possibility of there being only 1 object in the list. So h == he and h.next == None
-                # Then this goes through the loop and causes h to be None
-                # Either disallow the loop to run while h is null or consider any other alternatives
-                while h.next != he:
-                    h.face = face
-                    h = h.next
-                h.face = face
+                    face.halfedge = he
+                    face.halfedge.faces = face
+                while he.next != face.halfedge:
+                    he = he.next
+                    he.face = face
                 self.faces.append(face)
+
+           
     
-    def findRegionGivenSegment(self, section):
+    def findsection(self, section):
+        '''
+        finding a section
+
+        Parameters
+        ----------
+        section : vertex
+
+
+        Returns
+        -------
+        half edhe and start
+
+        '''
         v1 = section[0]
         v2 = section[1]
         start = self.findHalfEdge(v1, v2)
         h = start
-        while (not h.next == start):
+        while  h.next != start:
             print(h)
             h = h.next
         print(h, start)
 
-position = [(0, 5), (2, 5), (3, 0), (0, 0)]
-subdivide = [
+vertex = [(0, 5), (2, 5), (3, 0), (0, 0)]
+edge = [
       [(0, 5), (2, 5)],
       [(2, 5), (3, 0)],
       [(3, 0), (0, 0)],
@@ -147,15 +266,35 @@ subdivide = [
       [(0, 5), (3, 0)],
   ]
 
-dcel1 = DCEL()
-dcel1.printDCEL(position, subdivide)     
-#dcel1.findRegionGivenSegment([(3, 0), (0, 5)])      
+dcel1 = DCEL(vertex,edge)
+dcel1.printDCEL()     
+dcel1.findsection([(3, 0), (0, 5)])      
 
 class Room_Allocation(DCEL):
     def __init__(self,pathway_width):
         self.width = pathway_width
-    def rooms(self,position,subdivide):
-        dcel = DCEL()
-        dcel.printDCEL(position,subdivide)     
+    def rooms(self,vertex,edge):
+        '''
+        Room alloaction using DCEL
+
+        Parameters
+        ----------
+        vertex : Vertex
+            list of vertices
+        edge : Edge
+            list of edges
+
+        Returns
+        -------
+        dcel : DCEL
+            return room allocation considering the width of pathway
+
+        '''
+        newedgelist = []
+        for i in edge:
+            for j in i:
+                newedgelist.append((j[0]+self.width,j[1]))
+        dcel = DCEL(vertex,newedgelist)
+        dcel.printDCEL()     
         return dcel
         
